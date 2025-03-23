69bb664be9cadc3d6c56653dbf6a39844fed6919
citrushappy <venture.dalton@gmail.com>
2025-03-22 23:04:19 -0700

    rework uvmunmap

--- kernel/vm.c
+++ kernel/vm.c
@@ -180,35 +180,24 @@ mappages(pagetable_t pagetable, uint64 va, uint64 size, uint64 pa, int perm)
 void
 uvmunmap(pagetable_t pagetable, uint64 va, uint64 size, int do_free)
 {
-  uint64 a, last;
+  uint64 a;
   pte_t *pte;
-  uint64 pa;
 
-  a = PGROUNDDOWN(va);
-  last = PGROUNDDOWN(va + size - 1);
-  for(;;){
-    if((pte = walk(pagetable, a, 0)) == 0) // pte does not exist in pagetable (something went wrong with page table creation or address is wrong)
+  if((va % PGSIZE) != 0)
+    panic("uvmunmap: not aligned");
+
+  for(a = va; a < va + size; a += PGSIZE){
+    if((pte = walk(pagetable, a, 0)) == 0)
       panic("uvmunmap: walk");
-    if((*pte & PTE_V) == 0){ // uvmunmap: not mapped
-      printf("va=%p pte=%p\n", a, *pte);
-      *pte = 0;
-      if(a == last)
-        break;
-      a += PGSIZE;
-      pa += PGSIZE;
+    if((*pte & PTE_V) == 0)
       continue;
-    }
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
-      pa = PTE2PA(*pte);
+      uint64 pa = PTE2PA(*pte);
       kfree((void*)pa);
     }
     *pte = 0;
-    if(a == last)
-      break;
-    a += PGSIZE;
-    pa += PGSIZE;
   }
 }
 
